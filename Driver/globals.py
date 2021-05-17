e0c323857fc845d7d9300c6f0304c6bf07dd22bc
asudeeaydin <OhiO123>
2021-05-17 05:52:38 +0200

    Unshelving Working PID controller

--- Driver/globals.py
+++ Driver/globals.py
@@ -11,10 +11,14 @@ PATH_TO_EXPERIMENT_RECORDINGS = './ExperimentRecordings/'  # Path where the expe
 MOTOR_FULL_SCALE = 8192  # 7199 # with pololu motor and scaling in firmware #7199 # with original motor
 MOTOR_TYPE = 'POLOLU'
 
+# Angle unit conversion adc to radians: (ANGLE_TARGET + ANGLE DEVIATION - ANGLE_NORMALIZATION/2)/ANGLE_NORMALIZATION*math.pi
 # ANGLE_KP = ANGLE_KP*ANGLE_NORMALIZATION/math.pi
 ANGLE_AVG_LENGTH = 10  # adc routine in firmware reads ADC this many times quickly in succession to reduce noise
-ANGLE_NORMALIZATION = 2*math.pi/4095.0  # Range of angle values #
-ANGLE_UP = 2068
+ANGLE_NORMALIZATION = 4095 # Range of angle values #
+ANGLE_HANGING = 3475 # Value from sensor when pendulum is at stable equilibrium point
+ANGLE_DEVIATION = ANGLE_NORMALIZATION - ANGLE_HANGING # Angle deviation from goal
+ANGLE_NORMALIZATION_FACTOR = 2*math.pi/ANGLE_NORMALIZATION
+ANGLE_HANGING_NORMALIZATION = (ANGLE_DEVIATION + ANGLE_HANGING - ANGLE_NORMALIZATION/2)*ANGLE_NORMALIZATION_FACTOR # Should be equal to pi in radians
 
 # Position unit conversion adc to meters: POSITION_TARGET = POSITION_TARGET/POSITION_NORMALIZATION*TRACK_LENGTH
 # POSITION_KP = POSITION_KP*POSITION_NORMALIZATION/TRACK_LENGTH
@@ -24,7 +28,7 @@ POSITION_FULL_SCALE_N = int(POSITION_NORMALIZATION)/2 # Corrected position full 
 TRACK_LENGTH = 0.396 # Total usable track length in meters
 
 # Direction for measurement.py - n = 2 for right, n = 1 for left.
-n = 2
+n = 1
 
 ratio = 1.05
 
