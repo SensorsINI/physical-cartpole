6641eb4d75e78f9f3a2381bf4fe5fcd639c92a2d
asudeeaydin <asudeeaydin@gmail.com>
2021-06-09 02:20:52 +0200

    Help from utilities function removed since its now called from inside the controller

--- Driver/DriverFunctions/utilities.py
+++ Driver/DriverFunctions/utilities.py
@@ -5,30 +5,6 @@ But the ultimate aim is to have this file empty and all functions assigned to so
 
 import sys
 
-def help():
-    print("\n***********************************")
-    print("keystroke commands")
-    print("ESC quit")
-    print("k toggle control on/off (initially off)")
-    print("K trigger motor position calibration")
-    print("=/- increase/decrease (fine tune) angle deviation value")
-    print("[/] increase/decrease position target")
-    print("w/q angle proportional gain")
-    print("s/a angle derivative gain")
-    print("z/x angle smoothing")
-    print("r/e position proportional gain")
-    print("f/d position derivative gain")
-    print("c/v position smoothing")
-    print("l toggle logging data")
-    print("S/L Save/Load param values from disk")
-    print("D Toggle dance mode")
-    print(",./ Turn on motor left zero right")
-    print("m Toggle measurement")
-    print("j Switch joystick control mode")
-    print("b Print angle measurement from sensor")
-    print("***********************************")
-
-
 def calibrate(CartPoleInstance):
     print("Calibrating motor position.... ")
     if not CartPoleInstance.calibrate():
