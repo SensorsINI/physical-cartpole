fcf3015c580f39a716039b12bd50732531a68625
jeromejeannin <jeromemjeannin@gmail.com>
2021-10-27 01:38:03 +0200

    added Factory Firmware CubeIDE Project II

--- Firmware/FactoryFirmware CubeIDE/Debug/Startup/subdir.mk
+++ Firmware/FactoryFirmware CubeIDE/Debug/Startup/subdir.mk
@@ -16,5 +16,5 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Startup/%.o: ../Startup/%.s Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -DDEBUG -c -I"/Users/jeromejeannin/Dropbox/physical-cartpole/Firmware/Original CartPole CubeIDE/Src/SYSTEM/sys" -I"/Users/jeromejeannin/Dropbox/physical-cartpole/Firmware/Original CartPole CubeIDE/Src/SYSTEM/sys" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -DDEBUG -c -I"/Volumes/GoogleDrive/My Drive/LTC Semester Project/physical-cartpole/Firmware/FactoryFirmware CubeIDE/Src/SYSTEM/sys" -I"/Users/jeromejeannin/Dropbox/physical-cartpole/Firmware/Original CartPole CubeIDE/Src/SYSTEM/sys" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 
