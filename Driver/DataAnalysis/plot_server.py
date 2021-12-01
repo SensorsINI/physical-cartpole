81b2119a2415faaa4f586c12fa22880544da1681
Jerome Jeannin <jeromemjeannin@gmail.com>
2021-12-01 01:46:13 +0100

    histogramm for states in live plot

--- Driver/DataAnalysis/plot_server.py
+++ Driver/DataAnalysis/plot_server.py
@@ -12,21 +12,22 @@ print('Listening ...')
 connection = listener.accept()
 print(f'Connected to: {listener.last_accepted}')
 
-
-fig = plt.figure('Live Plot')
-fig.subplots_adjust(hspace=0.5)
-data = np.zeros((0, 7))
 labels = ['angle', 'angleD', 'position', 'positionD', 'calculatedMotorCmd']
-axs = []
-for i in range(data.shape[1]-2):
-    axs.append(fig.add_subplot(data.shape[1]-2, 1, i+1))
-received = True
+data = np.zeros((0, 7))
+
+
+fig, axs = plt.subplots(5, 2, figsize=(16,9), gridspec_kw={'width_ratios': [3, 1]})
+fig.subplots_adjust(hspace=0.5)
+fig.canvas.manager.set_window_title('Live Plot')
+
+
+received = False
 
 def animate(i):
     global data, fig, axs, labels, received
 
     # receive data from socket
-    while connection.poll(0.001):
+    while connection.poll(0.1):
         buffer = connection.recv()
 
         if buffer is 'reset':
@@ -41,18 +42,29 @@ def animate(i):
 
     if received:
         received = False
-        # plot received data
+        time = data[:, 0]
         colors = plt.rcParams["axes.prop_cycle"]()
 
         for i in range(data.shape[1]-2):
+            _data = data[:, i+1]
+            label = labels[i]
             color = next(colors)["color"]
-            axs[i].clear()
-            axs[i].set_title(f"Min: {data[:, i+1].min():.3f}, Max: {data[:, i+1].max():.3f}, Mean: {data[:, i+1].mean():.3f}, Std: {data[:, i+1].std():.3f}", size=8)
-            axs[i].plot(data[:, 0], data[:, i+1], label=labels[i], marker='.', color=color)
-            axs[i].legend(loc='upper right')
-            axs[i].grid(True, which='both', linestyle='-.', color='grey', linewidth=0.5)
+
+            # Timeline
+            axs[i, 0].clear()
+            axs[i, 0].set_title(f"Min: {_data.min():.3f}, Max: {_data.max():.3f}, Mean: {_data.mean():.3f}, Std: {_data.std():.5f}", size=8)
+            axs[i, 0].plot(time, _data, label=label, marker='.', color=color)
+            axs[i, 0].legend(loc='upper right')
+            axs[i, 0].grid(True, which='both', linestyle='-.', color='grey', linewidth=0.5)
             for b in data[data[:, -1] > 0, 0]:
-                axs[i].axvline(x=b, color='red', linestyle='--', alpha=0.7)
+                axs[i, 0].axvline(x=b, color='red', linestyle='--', alpha=0.7)
+
+            # Histogramm
+            if label is 'angleD':
+                axs[i, 1].hist(_data, bins=50, label=label, color=color)
+                axs[i, 1].set_ylabel('occurences')
+                axs[i, 1].set_xlabel(label)
+                axs[i, 1].grid(True, which='both', linestyle='-.', color='grey', linewidth=0.5)
 
 
 ani = animation.FuncAnimation(fig, animate, interval=50)
