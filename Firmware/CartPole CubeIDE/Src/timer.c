4ba0d7683b2e255dfecda45987cce771649fcc33
jeromejeannin <jeromemjeannin@gmail.com>
2021-12-14 19:57:56 +0100

    changable interrupt period

--- Firmware/CartPole CubeIDE/Src/timer.c
+++ Firmware/CartPole CubeIDE/Src/timer.c
@@ -2,25 +2,31 @@
 
 TIMER1_Callback cbTimer1;
 long timer_resets = 0;
-unsigned int _periodMS = 0;
+unsigned int periodMS = 0;
 
 // Timer base is configured to be 1MHz (resolution of 1us)
 // periodMS sets the period of the interrupt in ms (max value is 6553 ms)
-void TIMER1_Init(unsigned int periodMS)
+void TIMER1_Init(unsigned int _periodMS)
 {
-	_periodMS = periodMS;
-	periodMS = (periodMS * 1000) - 1;
+	periodMS = _periodMS;
 
 	cbTimer1 = 0;
 
-	RCC->APB2ENR	|= 1<<11;		// TIM1 clock enable    
- 	TIM1->ARR		 = periodMS;	// Setting counter automatic reload value  
-	TIM1->PSC		 = 72;		// The prescaler 7200 gets the count clock of 1Mhz.
-	TIM1->DIER		|= 1<<0;   		// Enable update interrupt				
-	TIM1->DIER		|= 1<<6;   		// Enable triggered interrupt	   
-	TIM1->CR1		|= 0x01;		// Enable timer
+	RCC->APB2ENR	|= 1<<11;					// TIM1 clock enable
+ 	TIM1->ARR		 = (periodMS * 1000) - 1;	// Setting counter automatic reload value
+	TIM1->PSC		 = 72;						// The prescaler 7200 gets the count clock of 1Mhz.
+	TIM1->DIER		|= 1<<0;   					// Enable update interrupt
+	TIM1->DIER		|= 1<<6;   					// Enable triggered interrupt
+	TIM1->CR1		|= 0x01;					// Enable timer
 	SYS_NVIC_Init(1, 1, TIM1_UP_IRQn, 2);
 }  
+
+void TIMER1_ChangePeriod(unsigned int _periodMS)
+{
+	timer_resets = 0;
+	periodMS = _periodMS;
+ 	TIM1->ARR		 = (periodMS * 1000) - 1;	// Setting counter automatic reload value
+}
 
 void TIMER1_SetCallback(TIMER1_Callback cb)
 {
@@ -29,12 +35,12 @@ void TIMER1_SetCallback(TIMER1_Callback cb)
 
 // Timer base is configured to be 1MHz (resolution of 1us)
 float TIMER1_getSystemTime() {
-    return ((float)timer_resets + (TIM1->SR ? 1.0 : 0.0)) * 1e-3 * _periodMS + ((float)TIM1->CNT) * 1e-6;
+    return ((float)timer_resets + (TIM1->SR ? 1.0 : 0.0)) * 1e-3 * periodMS + ((float)TIM1->CNT) * 1e-6;
 }
 
 // unsigned long max value: 4'294'967'295us = 4'294.967'295s ~ 60min
 unsigned int TIMER1_getSystemTime_Us() {
-    return (timer_resets + (TIM1->SR ? 1 : 0)) * 1000 * _periodMS + TIM1->CNT;
+    return (timer_resets + (TIM1->SR ? 1 : 0)) * 1000 * periodMS + TIM1->CNT;
 }
 
 // Timer 1 interrupt handler
