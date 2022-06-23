75f52fd6819a573e1353d6db043d6d4fd3dd3172
Ante <ante.maric@fer.hr>
2022-06-23 14:47:14 +0200

    Var removal for speedup + testing scripts

--- Driver/globals.py
+++ Driver/globals.py
@@ -56,7 +56,7 @@ ANGLE_HANGING_DEFAULT = True  # If True default ANGLE_HANGING is loaded for a re
 
 ANGLE_NORMALIZATION_FACTOR = 2 * math.pi / ANGLE_ADC_RANGE
 ANGLE_DEVIATION_FINETUNE = 0.134  # adjust from key commands such that upright angle error is minimized
-POLYFIT_ANGLED = True if CONTROLLER_NAME == 'mppi' and PREDICTOR in ['RNN', 'GP'] else False
+POLYFIT_ANGLED = True if 'mppi' in CONTROLLER_NAME and PREDICTOR in ['RNN'] else False
 
 ##### Position Conversion #####
 # Position unit conversion adc to meters: POSITION_TARGET_SOFTWARE = POSITION_TARGET_FIRMWARE*POSITION_NORMALIZATION_FACTOR
