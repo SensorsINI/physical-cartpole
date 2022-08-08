bc0df5114cb81d956c506b19d102241a76da13c5
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-08-08 13:25:05 +0200

    Removing try-except clauses and instead implementing solution from Frederik to use Cartpole Simulator both standalone and as physical cartpole submodule

--- Driver/control.py
+++ Driver/control.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(".", "Driver")))
+sys.path.insert(1, os.path.abspath(os.path.join(".", "Driver", "CartPoleSimulation")))
+
 import subprocess
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
