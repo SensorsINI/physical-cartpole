daa5be07545e7de296e635f7ceab3397acf60694
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-03-03 02:03:55 +0100

    New hanging value

--- Driver/globals.py
+++ Driver/globals.py
@@ -50,7 +50,7 @@ MOTOR_FULL_SCALE_SAFE = int(0.95 * MOTOR_FULL_SCALE)  # Including a safety const
 ANGLE_AVG_LENGTH = 32  # adc routine in firmware reads ADC this many times quickly in succession to reduce noise
 ANGLE_360_DEG_IN_ADC_UNITS = 4271.34  # Range of angle values #
 
-ANGLE_HANGING_POLOLU = 1057.0  # Value from sensor when pendulum is at stable equilibrium point
+ANGLE_HANGING_POLOLU = 1053.5  # Value from sensor when pendulum is at stable equilibrium point
 ANGLE_HANGING_ORIGINAL = 1046.75  # Value from sensor when pendulum is at stable equilibrium point
 
 ANGLE_HANGING_DEFAULT = True  # If True default ANGLE_HANGING is loaded for a respective cartpole when motor is detected at calibration
