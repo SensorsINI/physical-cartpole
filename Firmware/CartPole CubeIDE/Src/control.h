dc2b87d552ad2179082d1f2cdd9ccb98797457c1
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-10-24 04:52:05 +0200

    Slowdown disabled - what was the point of it?

--- Firmware/CartPole CubeIDE/Src/control.h
+++ Firmware/CartPole CubeIDE/Src/control.h
@@ -5,7 +5,7 @@
 #include "motor.h"
 
 #define CONTROL_LOOP_PERIOD_MS	        5
-#define CONTROL_SLOWDOWN				1
+#define CONTROL_SLOWDOWN				0
 #define CONTROL_MOTOR_MAX_SPEED         ((int)(0.95 * MOTOR_FULL_SCALE + 0.5))
 #define CONTROL_SYNC					false
 #define CONTROL_LATENCY_US				0
