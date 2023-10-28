3b0f6d14b0cbd4a3950143da7c57542d2bcbfd7b
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-10-28 15:55:46 +0200

    Trying to repair the mess after merge.(2)

--- Firmware/CartPole CubeIDE/Src/communication_with_PC.c
+++ Firmware/CartPole CubeIDE/Src/communication_with_PC.c
@@ -187,7 +187,8 @@ void prepare_message_to_PC_state(
 		int invalid_step,
 		unsigned long time_difference_between_measurement,
 		unsigned long timeMeasured,
-		unsigned long latency){
+		unsigned long latency,
+		unsigned short	latency_violation){
 
 	buffer[ 0] = SERIAL_SOF;
 	buffer[ 1] = CMD_STATE;
