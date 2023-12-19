eacb3a3a2b4dc7f62649bc975863a029df4d7b2b
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-12-19 23:45:40 +0100

    New files added: I added syscall.c and sysmem.c files to the proeject. I generated them by created the totaly new project with new version of cubeIDE, and copied these files over as described here https://community.st.com/t5/stm32cubeide-mcus/stm32cubeide-1-13-0-problem/m-p/587659#M20465 I did this because I had some warning after version upgrade.
    Also added files to make the project more modular and control.c less backed with control signal postprocessing and hardware_pid

---
+++ Firmware/CartPole CubeIDE/Src/control_signal_postprocessing.c
@@ -0,0 +1,40 @@
+/*
+ * control_signal_postprocessing.c
+ *
+ *  Created on: Dec 19, 2023
+ *      Author: marcinpaluch
+ */
+
+#include "hardware_bridge.h"
+
+int MOTOR_DYNAMICS_CORRECTED = true;
+float MOTOR_CORRECTION[3] = {4898.18, 168.09, 123.46};
+
+int control_signal_to_motor_command(float Q, float positionD) {
+	float actualMotorCmd_float = 0.0;
+    int actualMotorCmd = 0;
+
+    if (MOTOR_DYNAMICS_CORRECTED) {
+
+    	actualMotorCmd_float = Q * MOTOR_CORRECTION[0];
+        if (actualMotorCmd != 0) {
+            if (positionD > 0) {
+            	actualMotorCmd_float += MOTOR_CORRECTION[1];
+            } else if (positionD < 0) {
+            	actualMotorCmd_float -= MOTOR_CORRECTION[2];
+            }
+        }
+    } else {
+    	actualMotorCmd_float = Q * MOTOR_FULL_SCALE;
+    }
+
+    actualMotorCmd = (int)actualMotorCmd_float;
+
+    return actualMotorCmd;
+}
+
+void motor_command_safety_check(int* motor_command_ptr){
+    // Check if motor power in safe boundaries
+    if (*motor_command_ptr > MOTOR_FULL_SCALE_SAFE) *motor_command_ptr = MOTOR_FULL_SCALE_SAFE;
+    else if (*motor_command_ptr < -MOTOR_FULL_SCALE_SAFE) *motor_command_ptr = -MOTOR_FULL_SCALE_SAFE;
+}
---
+++ Firmware/CartPole CubeIDE/Src/control_signal_postprocessing.h
@@ -0,0 +1,14 @@
+/*
+ * control_signal_posprocessing.h
+ *
+ *  Created on: Dec 19, 2023
+ *      Author: marcinpaluch
+ */
+
+#ifndef CONTROL_SIGNAL_POSPROCESSING_H_
+#define CONTROL_SIGNAL_POSPROCESSING_H_
+
+int control_signal_to_motor_command(float Q, float positionD);
+void motor_command_safety_check(int* motor_command_ptr);
+
+#endif /* CONTROL_SIGNAL_POSPROCESSING_H_ */
