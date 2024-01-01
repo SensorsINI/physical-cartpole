d42c8c6ec6862adc28a40986b3f5adfbb910062f
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-01-01 23:54:16 +0100

    Renaming - towards easier integration with Zynq

---
+++ Firmware/CartPole CubeIDE/Src/STM/goniometer_stm.h
@@ -0,0 +1,9 @@
+#ifndef __GONIOMETER_STM_H_
+#define __GONIOMETER_STM_H_
+
+#include "sys.h"
+
+void            Goniometer_Init(void);
+unsigned short 	Goniometer_Read(void);
+
+#endif /*__GONIOMETER_STM_H_*/
