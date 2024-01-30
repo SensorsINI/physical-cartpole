8d069110eafd032231ab44ec0d689faffb8f07ba
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-01-30 16:49:52 +0100

    Zynq now with median filter on FPGA!

--- Firmware/CartPole CubeIDE/Src/Zynq/goniometer_zynq.c
+++ Firmware/CartPole CubeIDE/Src/Zynq/goniometer_zynq.c
@@ -1,11 +1,19 @@
 #include "goniometer_zynq.h"
-
+#include "math.h"
+#include <stdlib.h>
 
 XAdcPs XADC_Driver_Instance ;
 XAdcPs *XADC_Driver_Ptr = &XADC_Driver_Instance;
 
+XMedian_filter XMedian_filter_Instance;
+XMedian_filter * XMedian_filter_Ptr = &XMedian_filter_Instance;
+
 void Goniometer_Init(void)
 {
+
+	XMedian_filter_Initialize(XMedian_filter_Ptr, MEDIAN_FILTER_DEVICE_ID);
+	XMedian_filter_Set_window_size(XMedian_filter_Ptr, HARDWARE_MEDIAN);
+
     XAdcPs_Config* cfg = XAdcPs_LookupConfig ( XADC_DEVICE_ID ) ;
 
     XAdcPs_CfgInitialize(& XADC_Driver_Instance, cfg , cfg -> BaseAddress );
@@ -23,7 +31,8 @@ void Goniometer_Init(void)
 unsigned short Goniometer_Read(void)
 {
 	// Set conversion sequence		 
-	unsigned short volt_raw = XAdcPs_GetAdcData (XADC_Driver_Ptr, XADCPS_CH_AUX_MAX);
+//	unsigned short volt_raw = XAdcPs_GetAdcData(XADC_Driver_Ptr, XADCPS_CH_AUX_MAX);
+	unsigned short volt_raw = XMedian_filter_Get_median_o(XMedian_filter_Ptr);
 //	float volt_f = XSysMon_RawToExtVoltage(volt_raw);
 	return volt_raw/16;
 }
