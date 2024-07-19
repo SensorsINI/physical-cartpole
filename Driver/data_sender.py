4f719bc9942f252a030a12a0abc2345c577ecbb5
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-07-19 12:44:13 +0200

    Working first version

---
+++ Driver/data_sender.py
@@ -0,0 +1,41 @@
+import numpy as np
+from multiprocessing.connection import Client
+from datetime import datetime
+import time
+import pandas as pd
+from SI_Toolkit.load_and_normalize import load_data
+
+
+def main():
+    # Create a sample DataFrame
+    time.sleep(3)
+    path = './ExperimentRecordings/CPP_mpc__2024-07-01_00-52-52.csv'
+    df = pd.read_csv(path, comment='#')
+
+
+    df = df[['angle', 'angleD', 'position', 'positionD']]
+
+    # Use the server's local IP address
+    address = ('localhost', 6000)  # Replace with the server's local IP address if necessary
+    conn = Client(address)
+
+    try:
+        # Send the header
+        header = df.columns.tolist()
+        conn.send(header)
+        time.sleep(0.1)
+
+        # Send data line by line
+        for _, row in df.iterrows():
+            conn.send(row.values)
+            time.sleep(0.02)  # Wait for a short time before sending the next row
+
+        # Optionally, send a 'complete' message to indicate end of data
+        conn.send('complete')
+
+    finally:
+        conn.close()  # Close the connection
+
+
+if __name__ == '__main__':
+    main()
---
+++ Driver/plot_server.py
@@ -0,0 +1,80 @@
+from multiprocessing.connection import Listener
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib
+matplotlib.use('TkAgg')
+from datetime import datetime
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+address = ('0.0.0.0', 6000)     # Listen on all available interfaces
+listener = Listener(address)
+
+connection = listener.accept()
+print(f'Connected to: {listener.last_accepted}')
+
+# Initialize variables
+data = []
+header = None
+
+fig, axs = plt.subplots(5, 2, figsize=(16,9), gridspec_kw={'width_ratios': [3, 1]})
+fig.subplots_adjust(hspace=0.5)
+fig.canvas.manager.set_window_title('Live Plot')
+
+received = 0
+
+def animate(i):
+    global data, fig, axs, received
+
+    # receive data from socket
+    while connection.poll(0.01):
+        buffer = connection.recv()
+
+        if isinstance(buffer, list) and isinstance(buffer[0], str):  # Header received
+            global header
+            header = buffer
+            print(f'Header received: {header}')
+            continue
+        elif isinstance(buffer, np.ndarray):  # Data row received
+            data.append(buffer)
+            received += 1
+        elif isinstance(buffer, str) and buffer == 'complete':  # End of data
+            print('All data received.')
+            df = pd.DataFrame(data, columns=header)
+            print(df)
+            # Optionally process the DataFrame here
+
+
+        if received >= 10:
+            received = 0
+            # Your plotting logic here, for example:
+            if len(data) > 0:
+                df = pd.DataFrame(data, columns=header)
+                time = df.index
+                colors = plt.rcParams["axes.prop_cycle"]()
+
+                for i in range(len(header)):
+                    data_row = df.iloc[:, i]
+                    color = next(colors)["color"]
+
+                    # Timeline
+                    if i < 5:
+                        axs[i, 0].clear()
+                        axs[i, 0].set_title(f"Min={data_row.min():.3f}, Max={data_row.max():.3f}, Mean={data_row.mean():.3f}, Std={data_row.std():.5f}, N={data_row.size}", size=8)
+                        axs[i, 0].plot(time, data_row, label=header[i], marker='.', color=color, markersize=3, linewidth=0.2)
+                        axs[i, 0].legend(loc='upper right')
+                        axs[i, 0].grid(True, which='both', linestyle='-.', color='grey', linewidth=0.5)
+
+                    # Histogram
+                    if i < 5:
+                        axs[i, 1].clear()
+                        axs[i, 1].hist(data_row, bins=50, label=header[i], color=color)
+                        axs[i, 1].set_ylabel('Occurrences')
+                        axs[i, 1].set_xlabel(header[i])
+                        axs[i, 1].grid(True, which='both', linestyle='-.', color='grey', linewidth=0.5)
+
+ani = animation.FuncAnimation(fig, animate, interval=200)
+plt.show()
+print('Finished')
