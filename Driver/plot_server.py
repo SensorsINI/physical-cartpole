0970a0e7e7d98d97157be1c97893079019de2ba1
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-07-19 13:18:42 +0200

    More features

--- Driver/plot_server.py
+++ Driver/plot_server.py
@@ -9,6 +9,8 @@ import pandas as pd
 import seaborn as sns
 sns.set()
 
+LIVE_PLOT_KEEPSAMPLES = 100
+
 address = ('0.0.0.0', 6000)     # Listen on all available interfaces
 listener = Listener(address)
 
@@ -20,26 +22,37 @@ data = []
 header = None
 
 fig, axs = plt.subplots(5, 2, figsize=(16,9), gridspec_kw={'width_ratios': [3, 1]})
-fig.subplots_adjust(hspace=0.5)
+fig.subplots_adjust(hspace=0.8)
 fig.canvas.manager.set_window_title('Live Plot')
 
 received = 0
 
 def animate(i):
-    global data, fig, axs, received
+    global data, fig, axs, received, header
 
     # receive data from socket
     while connection.poll(0.01):
         buffer = connection.recv()
 
         if isinstance(buffer, list) and isinstance(buffer[0], str):  # Header received
-            global header
             header = buffer
             print(f'Header received: {header}')
             continue
         elif isinstance(buffer, np.ndarray):  # Data row received
             data.append(buffer)
             received += 1
+            data = data[-LIVE_PLOT_KEEPSAMPLES:]
+        elif buffer == 'reset':
+            data = []
+            print('\nLive Plot Reset\n\n\n\n')
+        elif buffer == 'save' and header is not None:
+            filepath = 'LivePlot' + str(
+                datetime.now().strftime('_%Y-%m-%d_%H-%M-%S'))
+            df = pd.DataFrame(data, columns=header)
+            df.to_csv(filepath + '.csv', index=False)
+            plt.savefig(filepath + '.pdf')
+            print(f'\nLive Plot saved: {filepath}.pdf')
+            print(f'Live Data saved: {filepath}.csv\n\n\n\n')
         elif isinstance(buffer, str) and buffer == 'complete':  # End of data
             print('All data received.')
             df = pd.DataFrame(data, columns=header)
@@ -72,7 +85,7 @@ def animate(i):
                         axs[i, 1].clear()
                         axs[i, 1].hist(data_row, bins=50, label=header[i], color=color)
                         axs[i, 1].set_ylabel('Occurrences')
-                        axs[i, 1].set_xlabel(header[i])
+                        axs[i, 1].set_title(header[i])
                         axs[i, 1].grid(True, which='both', linestyle='-.', color='grey', linewidth=0.5)
 
 ani = animation.FuncAnimation(fig, animate, interval=200)
