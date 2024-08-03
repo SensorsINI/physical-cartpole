8516a89ef1ee40f99891967d3a1d8b0b27f0df02
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-08-03 16:08:42 +0200

    Better hanging angle, probably changed by now anyway

--- Driver/globals.py
+++ Driver/globals.py
@@ -53,7 +53,7 @@ elif CHIP == 'ZYNQ':
     #   It is unclear if the difference comes from measuring it on different cartpoles
     #   or is due to imprecise voltage shifting which is required on Zybo
     #   Please think it through and adjust this comment appropriately.
-    ANGLE_HANGING_POLOLU = 1005.5  # Value from sensor when pendulum is at stable equilibrium point
+    ANGLE_HANGING_POLOLU = 1000.5  # Value from sensor when pendulum is at stable equilibrium point
     ANGLE_HANGING_ORIGINAL = 1008.5  # Value from sensor when pendulum is at stable equilibrium point
     POSITION_ENCODER_RANGE = 4695.0  # For new implementation with Zybo. FIXME: Not clear why different then for STM
 
