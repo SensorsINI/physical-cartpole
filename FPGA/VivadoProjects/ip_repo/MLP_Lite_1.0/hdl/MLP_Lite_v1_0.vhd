433ab2f99c2a4da7508e5522068dc19665a00d54
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-09-19 16:09:55 +0200

    Naming

--- FPGA/VivadoProjects/ip_repo/MLP_Lite_1.0/hdl/MLP_Lite_v1_0.vhd
+++ FPGA/VivadoProjects/ip_repo/MLP_Lite_1.0/hdl/MLP_Lite_v1_0.vhd
@@ -1,3 +1,5 @@
+-- MLP_Lite_v1_0.vhd
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
--- FPGA/VivadoProjects/ip_repo/MLP_Lite_1.0/hdl/MLP_Lite_v1_0_S00_AXI.vhd
+++ FPGA/VivadoProjects/ip_repo/MLP_Lite_1.0/hdl/MLP_Lite_v1_0_S00_AXI.vhd
@@ -1,3 +1,5 @@
+-- MLP_Lite_v1_0_S00_AXI.vhd
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
