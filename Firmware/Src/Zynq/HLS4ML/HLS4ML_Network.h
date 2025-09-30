90f5c50d9bf3b6a8b28af92b27669eb5a0fdeb72
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-09-30 14:17:18 +0200

    Working AXI Lite interface!

--- Firmware/Src/Zynq/HLS4ML/HLS4ML_Network.h
+++ Firmware/Src/Zynq/HLS4ML/HLS4ML_Network.h
@@ -3,9 +3,17 @@
 
 #include "xil_types.h"
 
-u32 HLS4ML_Network_Init(void);
-void HLS4ML_Network_Evaluate(UINTPTR NetworkInputBuffAddr, u32 network_input_length_in_bytes,
-							 UINTPTR NetworkOutputBuffAddr, u32 network_output_length_in_bytes);
+/* Auto-select already present in your code; keep it. */
+#if HLS4ML_INTERFACE == 0  /* AXI-Lite */
+  #ifndef HLS4ML_AXIL_BASE
+  #define HLS4ML_AXIL_BASE      XPAR_HARDWARE_ACCEL_MLP_AXI_LITE_INTERFA_0_BASEADDR
+  #endif
+  #define HLS4ML_AXIL_REG_CTRL  0x00u  /* bit0=start (W), bit1=done (R) */
+  #define HLS4ML_AXIL_REG_IN    0x04u  /* write next input (LSBs used)     */
+  #define HLS4ML_AXIL_REG_OUT   0x08u  /* read next output (LSBs contain)  */
+#endif
 
-
-#endif /* HLS4ML_NETWORK_H_ */
+u32  HLS4ML_Network_Init(void);
+void HLS4ML_Network_Evaluate(UINTPTR in_addr,  u32 in_len_bytes,
+                             UINTPTR out_addr, u32 out_len_bytes);
+#endif
