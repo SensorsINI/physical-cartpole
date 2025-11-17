bd886d9e13f215e171b18f1b6803d7f2b33cb8d3
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-11-17 16:18:54 +0100

    AXIS interface working after splitting!

--- FPGA/Interfaces/controller_axis.vhd
+++ FPGA/Interfaces/controller_axis.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------
---          Top module for controller with AXI Stream          --
---  This module has AXI Stream interface for input and output. --
---  Input AXI Stream is translated to the controller format.   --
---  Output controller format is translated to AXI Stream.      -- 
+--          AXI Stream interface for controller              --
+--  This module has AXIStream interface for input and output. --
+--  Input AXIStream is translated to the controller format.   --
+--  Output controller format is translated to AXIStream.      -- 
 ----------------------------------------------------------------
 --
 -- =====================================================================
@@ -15,7 +15,7 @@
 -- Edit the entity name on this line ONLY:
 -- >>> CONTROLLER_ADAPTER_ENTITY: controller_adapter_cp <<<
 --
--- Then update line ~161 in the architecture to match.
+-- Then update line ~169 in the architecture to match.
 -- =====================================================================
 library ieee;
 use ieee.std_logic_1164.all;
@@ -51,26 +51,22 @@ architecture RTL of controller_axis is
     constant BITS_PER_INPUT    : integer := BITS_PER_CONTROLLER_INPUT;
     constant NUM_OUTPUTS       : integer := CONTROLLER_OUTPUTS;
     constant BITS_PER_OUTPUT   : integer := BITS_PER_CONTROLLER_OUTPUT;
-    constant TOTAL_INPUT_BITS  : integer := NUM_INPUTS  * BITS_PER_INPUT;
+    
+    constant TOTAL_INPUT_BITS  : integer := NUM_INPUTS * BITS_PER_INPUT;
     constant TOTAL_OUTPUT_BITS : integer := NUM_OUTPUTS * BITS_PER_OUTPUT;
 
-    -- Core adapter ports
-    signal ap_rst          : std_logic;
-    signal ap_start        : std_logic;
-    signal ap_done         : std_logic;
-    signal ap_idle         : std_logic;
-    signal ap_ready_r      : std_logic;
-    signal ap_ready        : std_logic;
-    signal core_in_valid   : std_logic;
-    signal core_in_data_r  : std_logic_vector(TOTAL_INPUT_BITS - 1 downto 0);
-    signal core_in_data    : std_logic_vector(TOTAL_INPUT_BITS - 1 downto 0);
-    signal core_out_data   : std_logic_vector(TOTAL_OUTPUT_BITS - 1 downto 0);
-    signal core_out_valid  : std_logic; -- @suppress "signal core_out_valid is never read"
-    
-    signal read_inputs_r   : integer := 0;
-    signal read_inputs     : integer := 0;
-    signal write_outputs_r : integer := 0;
-    signal write_outputs   : integer := 0;
+    -- Core adapter signals
+    signal ap_rst                             : STD_LOGIC;
+    signal ap_start                           : STD_LOGIC;
+    signal ap_done                            : STD_LOGIC;
+    signal ap_idle                            : STD_LOGIC;
+    signal ap_ready_r, ap_ready               : STD_LOGIC;
+    signal core_in_valid                      : STD_LOGIC;
+    signal core_in_data_r, core_in_data       : STD_LOGIC_VECTOR(TOTAL_INPUT_BITS - 1 downto 0);
+    signal core_out_data                      : STD_LOGIC_VECTOR(TOTAL_OUTPUT_BITS - 1 downto 0);
+    signal core_out_valid                     : STD_LOGIC; -- @suppress "signal core_out_valid is never read"
+    signal read_inputs_r, read_inputs         : integer := 0;
+    signal write_outputs_r, write_outputs     : integer := 0;
 
     type state is (idle, read_axi_inputs, feed_controller, wait_controller_ready, wait_controller_done, write_axi_outputs);
     signal cs, ns : state;
@@ -99,19 +95,17 @@ begin
         end if;
     end process b_sync;
 
-    b_conv : process(cs, S_AXIS_TVALID, S_AXIS_TLAST, S_AXIS_TDATA, core_in_data_r, 
-                     read_inputs_r, write_outputs_r, ap_done, ap_ready_r, core_out_data, M_AXIS_TREADY)
+    b_conv : process(cs, S_AXIS_TVALID, S_AXIS_TLAST, S_AXIS_TDATA, core_in_data_r, read_inputs_r, write_outputs_r, ap_done, ap_ready_r, core_out_data, M_AXIS_TREADY)
     begin
-        ns              <= cs;
-        read_inputs     <= read_inputs_r;
-        write_outputs   <= write_outputs_r;
-        core_in_data    <= core_in_data_r;
-        core_in_valid   <= '0';
-        M_AXIS_TLAST    <= '0';
-        M_AXIS_TDATA    <= (others => '0');
-        M_AXIS_TVALID   <= '0';
-        ap_start        <= '0';
-        
+        ns             <= cs;
+        read_inputs    <= read_inputs_r;
+        write_outputs  <= write_outputs_r;
+        core_in_data   <= core_in_data_r;
+        core_in_valid  <= '0';
+        M_AXIS_TLAST   <= '0';
+        M_AXIS_TDATA   <= (others => '0');
+        M_AXIS_TVALID  <= '0';
+        ap_start       <= '0';
         case cs is
             when idle =>
                 if S_AXIS_TVALID = '1' then
@@ -120,7 +114,7 @@ begin
                     read_inputs                               <= read_inputs_r + 1;
                     -- Check if it is the last input data
                     if S_AXIS_TLAST = '1' then
-                        -- Move to feed_controller state
+                        -- Move to wait_controller_ready state
                         ns          <= feed_controller;
                         read_inputs <= 0;
                     else
@@ -132,12 +126,11 @@ begin
             when read_axi_inputs =>
                 if S_AXIS_TVALID = '1' then
                     -- Read the next input data
-                    core_in_data((BITS_PER_INPUT * read_inputs_r) + BITS_PER_INPUT - 1 downto (BITS_PER_INPUT * read_inputs_r)) 
-                        <= S_AXIS_TDATA(BITS_PER_INPUT - 1 downto 0);
-                    read_inputs <= read_inputs_r + 1;
+                    core_in_data((BITS_PER_INPUT * read_inputs_r) + BITS_PER_INPUT - 1 downto (BITS_PER_INPUT * read_inputs_r)) <= S_AXIS_TDATA(BITS_PER_INPUT - 1 downto 0);
+                    read_inputs                                                                                                  <= read_inputs_r + 1;
                     -- Check if it is the last input data
                     if S_AXIS_TLAST = '1' then
-                        -- Move to feed_controller state
+                        -- Move to wait_controller_ready state
                         ns          <= feed_controller;
                         read_inputs <= 0;
                     end if;
@@ -147,12 +140,12 @@ begin
                 -- Feed the controller with the input data
                 core_in_valid <= '1';
                 ap_start      <= '1';
-                -- Move to wait_controller_ready state
+                -- Move to wait_controller_done state
                 ns            <= wait_controller_ready;
 
             when wait_controller_ready =>
                 if ap_ready_r = '0' then
-                    -- Remain ap_start until ap_ready = '1'
+                    -- Remains ap_start until ap_ready = '1'
                     ap_start <= '1';
                 else
                     -- Move to wait_controller_done state
@@ -167,11 +160,10 @@ begin
 
             when write_axi_outputs =>
                 if M_AXIS_TREADY = '1' then
-                    -- Write the output data
-                    M_AXIS_TDATA(BITS_PER_OUTPUT - 1 downto 0) 
-                        <= core_out_data((BITS_PER_OUTPUT * write_outputs_r) + BITS_PER_OUTPUT - 1 downto (BITS_PER_OUTPUT * write_outputs_r));
-                    write_outputs <= write_outputs_r + 1;
-                    M_AXIS_TVALID <= '1';
+                    -- Write the first output data
+                    M_AXIS_TDATA(BITS_PER_OUTPUT - 1 downto 0) <= core_out_data((BITS_PER_OUTPUT * write_outputs_r) + BITS_PER_OUTPUT - 1 downto (BITS_PER_OUTPUT * write_outputs_r));
+                    write_outputs                              <= write_outputs_r + 1;
+                    M_AXIS_TVALID                              <= '1';
                     -- Check if it is the last output data
                     if write_outputs_r = NUM_OUTPUTS - 1 then
                         -- Move to idle state
@@ -185,14 +177,14 @@ begin
     end process b_conv;
 
     -- =====================================================================
-    -- Controller adapter instance
+    -- Controller adapter instance (wraps HLS core)
     -- =====================================================================
     -- *** TO CHANGE ADAPTER: Edit the entity name on the line below ***
     -- Current: controller_adapter_cp (1-output CartPole core)
     -- Options: controller_adapter_cp | controller_adapter_f1t
     -- See configuration section at top of file for details.
     -- =====================================================================
-    inst_adapter : entity work.controller_adapter_cp
+    inst_adapter: entity work.controller_adapter_cp
         port map(
             core_clk       => AXIS_ACLK,
             core_rst       => ap_rst,
@@ -205,8 +197,7 @@ begin
             core_out_valid => core_out_valid
         );
     
-    -- ap_idle is derived from adapter's ready signal
-    ap_idle <= ap_ready;
+    -- ap_idle is high when the adapter is ready for a new transaction
+    ap_idle <= not ap_start and not ap_done;
 
 end architecture RTL;
-
